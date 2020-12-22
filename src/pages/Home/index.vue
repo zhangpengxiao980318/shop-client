c9bb3ae0d2017fa2ebf1dd9ed1d18394af3388b2
xiaoxiao <2912598713@qq.com>
2020-12-22 21:31:05 +0800

    home静态页面

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="home">Home</div>
+  <div>
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <TodayRecommend></TodayRecommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
+  </div>
 </template>
 
 <script>
+import TypeNav from "./TypeNav";
+import ListContainer from "./ListContainer";
+import TodayRecommend from "./TodayRecommend";
+import Rank from "./Rank";
+import Like from "./Like";
+import Floor from "./Floor";
+import Brand from "./Brand";
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  components: {
+    TypeNav,
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
+};
 </script>
 
-<style lang="less" scoped>
-.home{
-  color: red;
-}
-</style>
+<style lang="less" scoped></style>
